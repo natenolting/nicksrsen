03003f56c93229e8249070a8ec520a09317e5cfa
Nick Srsen <nsrsen18@gmail.com>
2018-02-12 12:45:51 -0600

    changes

---
+++ assignments/February_6/endpoint.php
@@ -0,0 +1,50 @@
+<?php
+  // Set start of the sessions before any output is sent
+  session_start();
+  // Set all the post keys to session keys
+  foreach ($_POST as $key => $value) {
+      $_SESSION[$key] = $value;
+  }
+  if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $_SESSION['error_message'] = "Please provide a valid email address";
+      header("location: index.php");
+      exit();
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Submitted</title>
+  </head>
+  <body>
+    <div class="container">
+      <p>Hi <?php echo $_POST['name'] ?>, thanks for the comment. Here's what you sent:</p>
+      <table>
+      <thead>
+      <tr>
+      <th>Input</th>
+      <th>Value</th>
+      </tr>
+      </thead>
+  <tbody>
+      <?php
+      foreach ($_POST as $key => $val) {
+      echo "<tr>
+      <td>{$key}</td>
+      <td>{$val}</td>
+      </tr>";
+              }
+          ?>
+</tbody>
+      </table>
+    </div>
+  </body>
+</html>
+<?php
+
+  foreach (array_keys($_POST) as $key) {
+    if(isset($_SESSION[$key])) {
+      unset($_SESSION[$key]);
+    }
+  }
---
+++ assignments/February_6/index.php
@@ -0,0 +1,54 @@
+<?php
+  // set start of the sessions before any output is sent
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+<title>Forms</title>
+<style>
+  .error{
+    font-weight: 600px;
+    color: red;
+    font-family: italic;
+  }
+</style>
+</head>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<body>
+  <?php
+      if (isset($_SESSION['error_message'])) {
+        echo '<p class="error">' . $_SESSION['error_message'] . '</p>';
+        unset($_SESSION['error_message']);
+      }
+    ?>
+<form method="post" action="endpoint.php">
+  <fieldset>
+  <div>
+    <label for="name">Name:</label>
+      <input type="text" id="name" name="name" value="<?php echo (isset($_SESSION['name']) ? $_SESSION['name'] : null); ?>"
+       required="required"/>
+  </div>
+  <div>
+    <labeL for="email">Email:</label>
+      <input type="email" id="email" name="emial" value="<?php echo (isset($_SESSION['email']) ? $_SESSION['email'] : null); ?>"
+      required="required"/>
+  </div>
+  <div class="one">
+    <label for="phone number">Phone Number:</label>
+      <input type="phone number" id="phone number" name="phone number" required="required"/>
+  </div>
+  <div class="two">
+    <label for="comments">Comments:</label>
+
+      <textarea id="comment" name="comment" rows="5" columns="20" <?php echo (isset($_SESSION['message']) ? $_SESSION['message'] : null); ?>
+      required="required">Let me know what you think</textarea>
+  </div>
+  <div>
+      <input type="submit" value="Submit"/>
+</fieldset>
+    </form>
+
+</body>
+</html>
