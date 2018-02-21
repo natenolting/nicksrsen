2ca0c8b8a458fd48247d70ee3daeff3c572426bb
Nick Srsen <nsrsen18@gmail.com>
2018-02-21 14:43:13 -0600

    changes

--- assignments/February_15/index.php
+++ assignments/February_15/index.php
@@ -1,17 +1,6 @@
-<?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    $content = 'home';
-    if (isset($_GET['p']) && file_exists('pages/'. $_GET['p'] . '.php')) {
-       $content = $_GET['p'];
-    } elseif (isset($_GET['p']) && !file_exists('pages/'. $_GET['p'] . '.php')) {
-        $content = '404';
-    }
-?>
-
 <!DOCTYPE html>
 <html>
+<title>Template</title>
 <head>
   <link rel="stylesheet" href="css/style.css">
 </head>
