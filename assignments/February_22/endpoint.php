29c95527631972bdb4fc80198f21c761ba5229d8
Nick Srsen <nsrsen18@gmail.com>
2018-03-01 14:33:03 -0600

    changes

--- assignments/February_22/endpoint.php
+++ assignments/February_22/endpoint.php
@@ -1,49 +1,47 @@
 <?php
-require_once dirname(dirname(__DIR__)) . '/src/bootstrap.php';
+require 'vendor/autoload.php';
 use net\authorize\api\contract\v1 as AnetAPI;
 use net\authorize\api\controller as AnetController;
-define("AUTHORIZENET_LOG_FILE","phplog");
-$merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
-$merchantAuthentication->setName($AUTHORIZE_NET_API_LOGIN_ID);
-$merchantAuthentication->setTransactionKey($AUTHORIZE_NET_TRANSACTION_KEY);
-$refId = 'ref' . time();
-$creditCard = new AnetAPI\CreditCardType();
-$creditCard->setCardNumber($_POST['cc_number'] );
-$creditCard->setExpirationDate( $_POST['cc_expire']);
-$paymentOne = new AnetAPI\PaymentType();
-$paymentOne->setCreditCard($creditCard);
-$transactionRequestType = new AnetAPI\TransactionRequestType();
-$transactionRequestType->setTransactionType("authCaptureTransaction");
-$transactionRequestType->setAmount($_POST['payment_amount']);
-$transactionRequestType->setPayment($paymentOne);
-$request = new AnetAPI\CreateTransactionRequest();
-$request->setMerchantAuthentication($merchantAuthentication);
-$request->setRefId( $refId);
-$request->setTransactionRequest($transactionRequestType);
-$controller = new AnetController\CreateTransactionController($request);
-$response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
 
-$body = null;
-if ($response != null)
+define("AUTHORIZENET_LOG_FILE","phplog");
+
+$merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
+  $merchantAuthentication->setName("2HL5yPv66xM");
+  $merchantAuthentication->setTransactionKey("4G557fY8raKM77nj");
+  $refId = 'ref' . time();
+
+  $creditCard = new AnetAPI\CreditCardType();
+  $creditCard->setCardNumber("4111111111111111" );
+  $creditCard->setExpirationDate( "2038-12");
+  $paymentOne = new AnetAPI\PaymentType();
+  $paymentOne->setCreditCard($creditCard);
+
+  $transactionRequestType = new AnetAPI\TransactionRequestType();
+  $transactionRequestType->setTransactionType("authCaptureTransaction");
+  $transactionRequestType->setAmount(151.51);
+  $transactionRequestType->setPayment($paymentOne);
+  $request = new AnetAPI\CreateTransactionRequest();
+  $request->setMerchantAuthentication($merchantAuthentication);
+  $request->setRefId( $refId);
+  $request->setTransactionRequest($transactionRequestType);
+  $controller = new AnetController\CreateTransactionController($request);
+  $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
+
+  if ($response != null)
 {
-$tresponse = $response->getTransactionResponse();
-if (($tresponse != null) && ($tresponse->getResponseCode()=="1"))
-    {
-        $body .= "Charge Credit Card AUTH CODE : " . $tresponse->getAuthCode() . "\n";
-        $body .= "Charge Credit Card TRANS ID  : " . $tresponse->getTransId() . "\n";
-    }
-    else
-    {
-$body .= "Charge Credit Card ERROR :  Invalid response, error code ". $tresponse->getResponseCode() ."\n";
-$_SESSION['error'] = $body;
-        header( 'Location:index.php');
-        exit();
-    }
+  $tresponse = $response->getTransactionResponse();
+  if (($tresponse != null) && ($tresponse->getResponseCode()=="1"))
+  {
+    echo "Charge Credit Card AUTH CODE : " . $tresponse->getAuthCode() . "\n";
+    echo "Charge Credit Card TRANS ID  : " . $tresponse->getTransId() . "\n";
+  }
+  else
+  {
+    echo "Charge Credit Card ERROR :  Invalid response\n";
+  }
 }
 else
 {
-    $body .=  "Charge Credit Card Null response returned";
+  echo  "Charge Credit Card Null response returned";
 }
-$title = substr(__FILE__, strlen(__DIR__ . DIRECTORY_SEPARATOR), strlen(__FILE__));
-include __DIR__ . '/view.php';
 ?>
